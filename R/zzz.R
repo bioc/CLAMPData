8258c5a399449670fe0a81e129b3dbdd2e72aa99
David Mayer <david.mayer@cuanschutz.edu>
2025-09-12 14:42:40 -0400

    add template roxygen2 to create docs for ExperimentHub data functions

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,48 @@
+#' GSE164416_DP_htseq_counts_txt_gz
+#'
+#' A brief description of this dataset
+#'
+#' @format ## `GSE164416_DP_htseq_counts_txt_gz()`
+#' A data frame with x,xxx rows and xx columns:
+#' \describe{
+#'   \item{field1}{describe}
+#'   \item{field2}{describe}
+#'   \item{field3}{describe}
+#'   ...
+#' }
+#' @source <https://zenodo.org/records/17014313/files/GSE164416_DP_htseq_counts.txt.gz?download=1>
+"GSE164416_DP_htseq_counts_txt_gz"
+
+#' human_gene_v2_5_alz_h5
+#'
+#' A brief description of this dataset
+#'
+#' @format ## `human_gene_v2_5_alz_h5()`
+#' A data frame with x,xxx rows and xx columns:
+#' \describe{
+#'   \item{field1}{describe}
+#'   \item{field2}{describe}
+#'   \item{field3}{describe}
+#'   ...
+#' }
+#' @source <https://zenodo.org/records/17014313/files/human_gene_v2.5_alz.h5?download=1>
+"human_gene_v2_5_alz_h5"
+
+#' islets_metadata_csv
+#'
+#' A brief description of this dataset
+#'
+#' @format ## `islets_metadata_csv()`
+#' A data frame with x,xxx rows and xx columns:
+#' \describe{
+#'   \item{field1}{describe}
+#'   \item{field2}{describe}
+#'   \item{field3}{describe}
+#'   ...
+#' }
+#' @source <https://zenodo.org/records/17014313/files/islets_metadata.csv?download=1>
+"islets_metadata_csv"
+
 #' @importFrom utils read.csv
 .onLoad <- function(libname, pkgname) {
   fl <- system.file("extdata", "metadata.csv", package = pkgname)
