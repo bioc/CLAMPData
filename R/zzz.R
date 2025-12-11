6ed42dcfe83ecbea8f78fadfec9f577872de141e
msubirana <mb2subi@gmail.com>
2025-12-11 12:58:38 -0700

    added examples in functions and bup version to 0.99.2

--- R/zzz.R
+++ R/zzz.R
@@ -4,10 +4,7 @@
 #'
 #' @return A character string representing the local file path to the downloaded resource.
 #' @examples
-#' \dontrun{
-#'   counts_path <- CLAMPData::GSE164416_DP_htseq_counts_txt_gz()
-#'   # You can then use read.table or similar to load the data from this path
-#' }
+#' counts_path <- CLAMPData::GSE164416_DP_htseq_counts_txt_gz()
 #' @export
 GSE164416_DP_htseq_counts_txt_gz <- function() {
   ExperimentHub::ExperimentHub()[["EH10279"]]
@@ -20,10 +17,7 @@ GSE164416_DP_htseq_counts_txt_gz <- function() {
 #'
 #' @return A character string representing the local file path to the downloaded resource.
 #' @examples
-#' \dontrun{
-#'   h5_path <- CLAMPData::human_gene_v2_5_alz_h5()
-#'   # Use rhdf5::h5read to access the file
-#' }
+#' h5_path <- CLAMPData::human_gene_v2_5_alz_h5()
 #' @export
 human_gene_v2_5_alz_h5 <- function() {
   ExperimentHub::ExperimentHub()[["EH10280"]]
@@ -36,10 +30,7 @@ human_gene_v2_5_alz_h5 <- function() {
 #'
 #' @return A character string representing the local file path to the downloaded resource.
 #' @examples
-#' \dontrun{
-#'   metadata_path <- CLAMPData::islets_metadata_csv()
-#'   # You can then use read.csv to load the data
-#' }
+#' metadata_path <- CLAMPData::islets_metadata_csv()
 #' @export
 islets_metadata_csv <- function() {
   ExperimentHub::ExperimentHub()[["EH10281"]]
